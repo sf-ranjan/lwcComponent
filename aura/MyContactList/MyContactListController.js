09c603bfaef803a6638d47c1f0812479f53df435
Deep Ranjan <deepranjan@Deeps-MacBook-Pro.local>
2023-11-19 23:05:11 +0530

    new changes

--- aura/MyContactList/MyContactListController.js
+++ aura/MyContactList/MyContactListController.js
@@ -1,11 +1,11 @@
 ({
   myAction: function (component, event, helper) {
     component.set("v.Columns", [
-        {label:"First Name", fieldName:"FirstName", type:"text"},
-        {label:"Last Name", fieldName:"LastName", type:"text"},
-        {label:"Phone", fieldName:"Phone", type:"phone"}
-    ]); 
-    var action = component.get("c.getContacts");   
+      { label: "First Name", fieldName: "FirstName", type: "text" },
+      { label: "Last Name", fieldName: "LastName", type: "text" },
+      { label: "Phone", fieldName: "Phone", type: "phone" }
+    ]);
+    var action = component.get("c.getContacts");
     action.setParams({
       recordId: component.get("v.recordId")
     });
